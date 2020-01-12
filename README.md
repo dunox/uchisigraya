531b84179bd04b66504d9d63db89b0652ba150be
Andriy <34597632+dunox@users.noreply.github.com>
2020-01-12 20:55:22 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Wallpapers
+test task with wallpaper site
+<br>
+На работy ушло примерно 10-12 часов
+<br>
+<strong>Ссылка на готовый сайт</strong> - https://dunox.github.io/
---
+++ main.js
@@ -0,0 +1,13 @@
+$(function(){
+    $('.main__slider').slick({
+        infinite: true,
+        speed: 500,
+        fade: true,
+        cssEase: 'linear',
+        prevArrow: '<img class="slider-arrows slider-arrows__left" src="app/images/arrow-left.png" alt=""></img>',
+        nextArrow: '<img class="slider-arrows slider-arrows__right" src="app/images/arrow-right.png" alt=""></img>',
+        dots: true,
+      });
+
+    
+});
